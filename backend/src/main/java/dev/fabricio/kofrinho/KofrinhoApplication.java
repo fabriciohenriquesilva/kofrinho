a3aa8379e56ef1de23c66776e39a65f016879c1a
Fabricio Silva <fabricionhsilva@gmail.com>
2026-02-15 10:48:55 -0300

    create backend project

---
+++ backend/src/main/java/dev/fabricio/kofrinho/KofrinhoApplication.java
@@ -0,0 +1,13 @@
+package dev.fabricio.porkinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KofrinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorkinhoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/fabricio/kofrinho/KofrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fabricio.porkinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KofrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
