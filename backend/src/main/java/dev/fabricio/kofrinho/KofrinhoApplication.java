ea6d958875fcbe3a7c015043034faa39f51a743b
Fabricio Silva <fabricionhsilva@gmail.com>
2026-02-15 20:21:20 -0300

    feat: setup inicial da aplicação

--- backend/src/main/java/dev/fabricio/kofrinho/KofrinhoApplication.java
+++ backend/src/main/java/dev/fabricio/kofrinho/KofrinhoApplication.java
@@ -1,4 +1,4 @@
-package dev.fabricio.porkinho;
+package dev.fabricio.kofrinho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KofrinhoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PorkinhoApplication.class, args);
+		SpringApplication.run(KofrinhoApplication.class, args);
 	}
 
 }
