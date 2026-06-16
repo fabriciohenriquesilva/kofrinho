8c387681c018c4daf3e1f7ceb409523cd78cb14b
fabricio <fabricionhsilva@gmail.com>
2026-06-15 22:59:22 -0300

    feat: adição do componente sidemenu

---
+++ frontend/src/app/shared/sidebar/sidebar.ts
@@ -0,0 +1,35 @@
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+    selector: 'sidebar',
+    imports: [RouterLink],
+    templateUrl: './sidebar.html',
+    styleUrl: './sidebar.scss',
+    standalone: true,
+})
+export class Sidebar {
+
+    isCollapsed = false;
+
+    toggleMenu(): void {
+        this.isCollapsed = !this.isCollapsed;
+    }
+
+    menuItems: MenuItem[] = [
+        {id: 1, label: 'Dashboard', path: 'home', icon: 'ph ph-house', active: true},
+        {id: 2, label: 'Cadastros', path: 'cadastros', icon: 'ph ph-folder'},
+        {id: 3, label: 'Lançamentos', path: 'lancamentos', icon: 'ph ph-money'},
+        {id: 4, label: 'Veículos', path: 'veiculos', icon: 'ph ph-car'},
+        {id: 5, label: 'Relatórios', path: 'reports', icon: 'ph ph-presentation-chart'},
+        {id: 6, label: 'Configurações', path: 'configs', icon: 'ph ph-gear'},
+    ];
+}
+
+interface MenuItem {
+    id: number;
+    label: string;
+    icon: string;
+    active?: boolean;
+    path: string;
+}
